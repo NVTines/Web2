68317aaa31adf02ad672420473859983c4330c0a
NVTines <ngovantin280702@gmail.com>
2024-03-31 16:26:09 +0700

    update(31/3)

---
+++ admin_pages/database.php
@@ -0,0 +1,34 @@
+<?php
+    class database{
+        private $servername = "localhost";
+        private $username = "root";
+        private $password = "";
+        private $dbname = "website_2"; 
+        private $conn;
+
+        function __construct() {
+            $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+            // if($this->conn){
+            //     echo "Ket noi thanh cong";
+            // }else{
+            //     echo "Ket noi that bai";
+            // }
+        }
+
+        function insert_data($sql){
+            if ($this->conn->query($sql) === TRUE) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        function get_data($sql){
+            return $this->conn->query($sql);
+        }
+        function close_dtb(){
+            $this->conn->close();
+        }
+
+    }
+
+?>
