497c345b3341edd73b35766655206e68670690ce
htuan2800 <huynhngoctuan48@gmail.com>
2024-04-14 18:57:35 +0700

    Update cart.php

--- pages/cart.php
+++ pages/cart.php
@@ -1,36 +1,41 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd" crossorigin="anonymous">
+
+
 <form action="xuly.php" method="get" onsubmit="layttHD()" id="hoadon" style="position:fixed;background-color:whitesmoke;z-index: -1;opacity:0;height:100%;width:500px;border:solid black 2px">
     <a href="#" onclick="outHD();"><input type="button" style="padding:0 3px;float: right;font-size:30px;background: #FFFFFF;border: #FFFFFF;cursor: pointer" class="material-icons" value="backspace"></a>
-    <h1 style="text-align: center;margin-bottom:50px"><i>Payment Form</i></h1>
+    <h1 style="text-align: center;margin-bottom:10px"><i>Payment Form</i></h1>
 
     <table>
         <tr>
             <td>Name</td>
             <td>
-                <input id="nnhoten" style="width:300px" type="text" placeholder="" name="" />
+                <input id="nnhoten" style="width:300px; height:20px;" type="text" placeholder="" name="" />
             </td>
         </tr>
         <tr>
             <td>Phone Number</td>
             <td>
-                <input id="nnsdt" style="width:300px" type="phone" placeholder="" />
+                <input id="nnsdt" style="width:300px; height:20px;" type="phone" placeholder="" />
             </td>
         </tr>
         <tr>
             <td>Email</td>
             <td>
-                <input id="nnmail" style="width:300px" type="email" placeholder="" />
+                <input id="nnmail" style="width:300px; height:20px;" type="email" placeholder="" />
             </td>
         </tr>
         <tr>
             <td>Province-City</td>
             <td>
-                <input id="nncity" style="width:300px" type="" placeholder="" />
+                <input id="nncity" style="width:300px; height:20px;" type="" placeholder="" />
             </td>
         </tr>
         <tr>
             <td>Address</td>
             <td>
-                <input id="nndiachi" style="width:300px" type="address" placeholder="" />
+                <input id="nndiachi" style="width:300px; height:20px;" type="address" placeholder="" />
             </td>
         </tr>
         <tr>
@@ -47,7 +52,7 @@
         <tr>
             <td>Date of Birth</td>
             <td>
-                <input id="nnngaysinh" style="width:300px" type="date" />
+                <input id="nnngaysinh" style="width:300px; height:20px;" type="date" />
             </td>
         </tr>
         <tr>
@@ -65,7 +70,7 @@
         <tr>
             <td>Note</td>
             <td>
-                <textarea id="note" rows="5" col="17" placeholder=""></textarea>
+                <textarea id="note" rows="2" col="17" placeholder=""></textarea>
             </td>
         </tr>
         <tr>
@@ -92,7 +97,65 @@
     </table>
 </form>
 <div class="small-container">
-    <table id="dssp">
+
+
+    <div class="card border-0 shadow-sm mb-4">
+        <div class="card-body d-flex justify-content-between align-items-center">
+            <div class="table-responsive-md" style="height:300px; overflow-y:scroll; width: 100%;">
+                <table class="table table-hover border text-cennter">
+                    <thead class="">
+                        <tr class="bg-dark text-light">
+                            <th scope="col">Image</th>
+                            <th scope="col">ProductName</th>
+                            <th scope="col">Size</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Color</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody id="cart-data">
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div style="margin-right: 40px;">
+        <div class="total-price">
+            <table id="totalHoaDon">
+                <tr>
+                    <td>Subtotal</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Tax</td>
+                    <td>10%</td>
+                </tr>
+                <tr>
+                    <td>Total</td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+        <div class="check">
+            <table>
+                <tr>
+                    <td><a href="#" onclick="reloadHD()" style="color: white;background: #ff523b;padding: 10px;font-weight: bold;cursor: pointer;margin: 20px; text-decoration: none;">BACK</a></td>
+                    <td style="text-align: center">
+                        <a href="#" onclick="hienHD()" style="color: white;background: #ff523b;padding: 10px;font-weight: bold;cursor: pointer;margin: 20px; text-decoration: none;">
+                            Proceed to checkout
+                        </a>
+                    </td>
+                    <td><a href="#" onclick="bill()" style="color: white;background: #ff523b;padding: 10px;font-weight: bold;cursor: pointer;margin: 20px; text-decoration: none;">EXIT &#10004;</a></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+
+
+    <!-- <table id="dssp">
         <tr>
             <th>Product</th>
             <th>ID Product</th>
@@ -113,33 +176,35 @@
             <td></td>
         </tr>
     </table>
+    
     <div class="total-price">
-        <table id="totalHoaDon">
-            <tr>
-                <td>Subtotal</td>
-                <td></td>
-            </tr>
-            <tr>
-                <td>Tax</td>
-                <td>10%</td>
-            </tr>
-            <tr>
-                <td>Total</td>
-                <td></td>
-            </tr>
-        </table>
-    </div>
-    <div class="check">
-        <table>
-            <tr>
-                <td><a href="#" onclick="reloadHD()" style="color: white;background: #ff523b;padding: 10px;font-weight: bold;cursor: pointer;margin: 20px;">BACK</a></td>
-                <td style="text-align: center">
-                    <a href="#" onclick="hienHD()" style="color: white;background: #ff523b;padding: 10px;font-weight: bold;cursor: pointer;margin: 20px;">
-                        PROCEED TO CHECKOUT
-                    </a>
-                </td>
-                <td><a href="#" onclick="bill()" style="color: white;background: #ff523b;padding: 10px;font-weight: bold;cursor: pointer;margin: 20px;">VIEW &#10004;</a></td>
-            </tr>
-        </table>
-    </div>
+            <table id="totalHoaDon">
+                <tr>
+                    <td>Subtotal</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Tax</td>
+                    <td>10%</td>
+                </tr>
+                <tr>
+                    <td>Total</td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+        <div class="check">
+            <table>
+                <tr>
+                    <td><a href="#" onclick="reloadHD()" style="color: white;background: #ff523b;padding: 10px;font-weight: bold;cursor: pointer;margin: 20px;">BACK</a></td>
+                    <td style="text-align: center">
+                        <a href="#" onclick="hienHD()" style="color: white;background: #ff523b;padding: 10px;font-weight: bold;cursor: pointer;margin: 20px;">
+                            Proceed to checkout
+                        </a>
+                    </td>
+                    <td><a href="#" onclick="bill()" style="color: white;background: #ff523b;padding: 10px;font-weight: bold;cursor: pointer;margin: 20px;">EXIT &#10004;</a></td>
+                </tr>
+            </table>
+        </div> -->
+
 </div>
