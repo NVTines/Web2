9a74e6c80fe4c9c598ab43e423c2b4a53da247cb
htuan2800 <huynhngoctuan48@gmail.com>
2024-04-15 22:20:58 +0700

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -37,25 +37,25 @@
     });
 </script>
 
-<div style="border-bottom: #CDCDCD solid 1px">
+<div>
     <a onclick="out();"><input type="button"  style="padding:0 3px;float: right;font-size:30px;background: #FFFFFF;border: #FFFFFF;cursor: pointer" class="material-icons" value="backspace"></a>
     <h2>MY ACCOUNT</h2>
 </div>
-<div style="padding: 40px;border-bottom: #CDCDCD solid 1px">
+<div style="padding: 40px;border-bottom: #CDCDCD solid 1px;font-size:18px;">
     <div id="ttdn">
         <form id="login-form" class="login-form" method="POST" >
             <label for="tendn" class="label-usr" id="label-usr"><b>Username</b></label></br>
-            <input required type="text" id="tendn" class="tendn"/></br>
+            <input required type="text" id="tendn" class="tendn" /></br>
             <label for="mk" class="label-pwd" id="label-pwd"><b>Password</b></label></br>
             <input required type="password" id="mk" class="mk"/></br>
             <button type="submit" class="login-btn" id="login-btn" style="cursor: pointer;font-size:20px;background-color:black;color:white;padding:10px 30px;text-align: center;display:block;margin-top:20px">Sign in</button>
         </form>
     </div>
 </div>
-<div style="padding:30px 40px">
+<div style="padding:30px 40px; font-size:18px;">
     <div style="font-weight: bold">I DON'T HAVE AN ACCOUNT</div>
     <div style="margin-top: 20px;margin-bottom:50px">Enjoy added benefits and a richer experience by creating a personal account</div>
 <div style="text-align: center	">
-        <a style="cursor: pointer;font-size:20px;background-color:black;color:white;padding:10px 30px;text-align: center;display:block" onclick="CreateAccountBoard()">Create an Account</a>
+        <a style="cursor: pointer;font-size:20px;background-color:black;color:white;padding:10px 30px;text-align: center;display:block;font-weight:bold" onclick="CreateAccountBoard()">Create An Account</a>
     </div>
 </div>
