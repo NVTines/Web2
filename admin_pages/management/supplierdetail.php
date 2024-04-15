cd139d21b2d22a05d87859d13643dbbbb3d73cc1
htuan2800 <huynhngoctuan48@gmail.com>
2024-04-15 22:19:36 +0700

    Update supplierdetail.php

--- admin_pages/management/supplierdetail.php
+++ admin_pages/management/supplierdetail.php
@@ -2,20 +2,25 @@
 $id = $_GET["id"];
 $sql = "SELECT * from supplier where SupplierID = '$id'";
 echo 
-    '<div class="col-div-8" style="margin-bottom:50px;">
+'<div class="col-div-8" style="margin:20px 0px;">
+    <div class="box-8">
+        <div class="manage-name-btn" onclick="invinbox()">&#9776;QUẢN LÝ NHÀ CUNG CẤP</div>
+    </div>
+</div>
+<div class="col-div-8" style="margin-bottom:50px;">
     <div class="box-8">
         <div class="content-box">
-        <table id="showlist">
-            <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Address</th>
-            <th>Phone</th>
-            <th>Fax</th>
-            <th>Status</th>
-            <th></th>
-            </tr>
-            <tr>';
+            <table id="showlist">
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Phone</th>
+                    <th>Fax</th>
+                    <th>Status</th>
+                    <th></th>
+                </tr>
+                <tr>';
         if($results = $db->get_data($sql)){
             while($rows = $results->fetch_assoc()){
             echo 
@@ -27,33 +32,33 @@ echo
             <td>'.$rows["status"].'</td>
             <td>
                 <a href="admin.php?key=ncc">
-                <button class="quit_btn" id="quit_btn">
-                    <i class="fa-solid fa-circle-xmark"></i>
-                </button>
+                    <button class="quit_btn" id="quit_btn">
+                        <i class="fa-solid fa-circle-xmark"></i>
+                    </button>
                 </a>
             </td>';            
             }    
         }
         echo 
-            '</tr>
-        </table>
+                '</tr>
+            </table>
         </div>
     </div>
-    </div>';
+</div>';
 $sql = "SELECT p.* FROM product as p, supplierdetail as sd WHERE sd.SupplierID = '$id' AND sd.ProductID = p.ProductID";
 echo 
-    '<div class="col-div-8">
+'<div class="col-div-8">
     <div class="box-8">
         <div class="content-box">
-        <table id="showlist">
-            <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Image</th>
-            <th>Description</th>
-            <th>Status</th>
-            </tr>
-            <tr>';
+            <table id="showlist">
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Image</th>
+                    <th>Description</th>
+                    <th>Status</th>
+                </tr>
+                <tr>';
 if($results = $db->get_data($sql)){
     while($rows = $results->fetch_assoc()){
     echo 
@@ -65,9 +70,8 @@ if($results = $db->get_data($sql)){
     }
 }
 echo 
-            '</tr>
-        </table>
+                '</tr>
+            </table>
         </div>
-    </div>
     </div>
 </div>';
