b4c4c9e6a01ff2e1d576ad5533f55c75373f7d64
htuan2800 <huynhngoctuan48@gmail.com>
2024-04-15 22:19:30 +0700

    Update donhang.php

--- admin_pages/management/donhang.php
+++ admin_pages/management/donhang.php
@@ -7,13 +7,6 @@
           <span onclick="invinbox()" style="font-size: 30px; cursor: pointer; color: white" class="nav2">&#9776;QUẢN LÝ
             ĐƠN HÀNG</span>
         </div>
-
-        <div class="col-div-6">
-          <div class="profile">
-            <img src="user.png" class="pro-img" />
-            <p class="profile">MBKT<span>DESIGNER</span></p>
-          </div>
-        </div>
         <div class="clearfix">
 
         </div>
