e2ce50e3f00ac8975acb6df1df7aee3aee7e3e3c
htuan2800 <huynhngoctuan48@gmail.com>
2024-04-19 01:54:17 +0700

    Create bill.php

---
+++ pages/functions/bill.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+require_once __DIR__ . "/../../database.php";
+$dtb = new database();
+if (isset($_POST['add_bill'])) {
+    $data = $dtb->filteration($_POST);
+    $res1 = $dtb->select("SELECT * FROM `customer` WHERE `UserID`=?", [$_SESSION['UserID']], 'i');
+    if (mysqli_num_rows($res1) > 0) {
+        $row1 = $res1->fetch_assoc();
+        $res2 = $dtb->select("SELECT * FROM `cart` WHERE `UserID`=?", [$_SESSION['UserID']], 'i');
+        if (mysqli_num_rows($res2) > 0) {
+            $row2 = $res2->fetch_assoc();
+            $res3 = $dtb->select("SELECT * FROM `cartdetails` WHERE `CartID`=?", [$row2['CartID']], 'i');
+            $total = 0;
+            while ($row3 = mysqli_fetch_assoc($res3)) {
+                $total += $row3['Quantity'] * $row3['UnitPrice'];
+
+                $billDetails[] = array(
+                    'ProductID' => $row3['ProductID'],
+                    'Quantity' => $row3['Quantity'],
+                    'UnitPrice' => $row3['UnitPrice']
+                );
+            }
+            $createDate = date('Y-m-d H:i:s');
+            //insert bill
+            $is1 = "INSERT INTO `bill`(`CustomerID`,`CreateTime`, `Total`,`delivery`,`payment`, `status`) VALUES (?,?,?,?,?,?)";
+            $vl1 = [$row1['CustomerID'], $createDate, $total,$data['delivery'],$data['pttt'], "Đã Đặt"];
+            if ($dtb->insert($is1, $vl1, 'isdsss')) {
+                $bill_id = mysqli_insert_id($dtb->get_conn()); // sẽ chứa giá trị ID của phòng vừa được thêm vào
+                foreach ($billDetails as $billDetail) {
+                    $is2 = "INSERT INTO `billdetail`(`BillID`, `ProductID`, `Quantity`, `Unitprice`) VALUES (?,?,?,?)";
+                    $vl2 = [$bill_id, $billDetail['ProductID'], $billDetail['Quantity'], $billDetail['UnitPrice']];
+                    $resIS = $dtb->insert($is2, $vl2, 'iiid');
+
+                    $res4=$dtb->select("SELECT * FROM `product` WHERE ProductID=?",[$billDetail['ProductID']],'i');
+                    if(mysqli_num_rows($res4)>0){
+                        $row4=$res4->fetch_assoc();
+                        $quantity=$row4['Quantity']-$billDetail['Quantity'];
+                        $resUP=$dtb->update("UPDATE `product` SET `Quantity`=? WHERE `ProductID`=?",[$quantity,$billDetail['ProductID']],'ii');
+                    }
+                }
+            }
+
+            $res5 = $dtb->delete("DELETE FROM `cartdetails` WHERE `CartID`=?", [$row2['CartID']], 'i');
+            $res6 = $dtb->delete("DELETE FROM `cart` WHERE `UserID`=?", [$_SESSION['UserID']], 'i');
+            if ($res5 || $res6) {
+                echo 1;
+            } else {
+                echo 0;
+            }
+        } else {
+            echo 'GHR';
+        }
+    } else {
+        echo 'Server Down!';
+    }
+}
+
+
+?>
