6ed5868135a04ee545d996a49d1c86fba378df64
htuan2800 <huynhngoctuan48@gmail.com>
2024-04-18 21:14:06 +0700

    Update dashboard.php

--- admin_pages/functions/dashboard.php
+++ admin_pages/functions/dashboard.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__."/../../database.php";
+require_once __DIR__ . "/../../database.php";
 session_start();
 $dtb = new database();
 if (isset($_POST['countAll'])) {
@@ -7,12 +7,12 @@ if (isset($_POST['countAll'])) {
     $countSP = 0;
     $countDHDD = 0;
     $countSPBD = 0;
-    $percent=0;
-    
+    $percent = 0;
+
     // Lấy số lượng khách hàng
     $res1 = $dtb->mysqli_query("SELECT COUNT(*) AS `customer_count` FROM `customer`");
     if ($res1) {
-        if($row1 = $res1->fetch_assoc()){
+        if ($row1 = $res1->fetch_assoc()) {
             $countKH = $row1['customer_count'];
         }
     }
@@ -20,55 +20,52 @@ if (isset($_POST['countAll'])) {
     // Lấy số lượng sản phẩm
     $res2 = $dtb->mysqli_query("SELECT COUNT(*) AS `product_count` FROM `product`");
     if ($res2) {
-        if($row2 = $res2->fetch_assoc())
-        {
+        if ($row2 = $res2->fetch_assoc()) {
             $countSP = $row2['product_count'];
         }
     }
 
     // Lấy hóa đơn
-    $res3=$dtb->select("SELECT COUNT(*) AS `bill_count` FROM `bill` WHERE `status`=?",['Đã Nhận Hàng'],'s');
-    if($res3){
-        if($row3=$res3->fetch_assoc()){
-            $countDHDD=$row3['bill_count'];
+    $res3 = $dtb->select("SELECT COUNT(*) AS `bill_count` FROM `bill` WHERE `status`=?", ['Đã Nhận Hàng'], 's');
+    if ($res3) {
+        if ($row3 = $res3->fetch_assoc()) {
+            $countDHDD = $row3['bill_count'];
         }
     }
 
     //lấy sản phẩm bán được
-    $res4=$dtb->mysqli_query("SELECT SUM(`Quantity`) AS `quantity` FROM `billdetail`");
-    if($res4){
-        if($row3=$res4->fetch_assoc() && $res4->num_rows == 0)
-            $countSPBD=0;
-        else if($row3=$res4->fetch_assoc() && $res4->num_rows > 0)
-            $countSPBD=$row3['quantity'];
+    $res4 = $dtb->mysqli_query("SELECT SUM(`Quantity`) AS `quantity` FROM `billdetail`");
+    if ($res4) {
+        if ($row3 = $res4->fetch_assoc())
+            $countSPBD = $row3['quantity'];
+        else 
+            $countSPBD = 0;
     }
 
     //tỉ lệ 
-    $res5=$dtb->selectAll('bill');
-    $hd=0;
-    $hdtc=0;
-    while($row5=$res5->fetch_assoc()){
-        if($row5['status']=='Đã Nhận Hàng'){
+    $res5 = $dtb->selectAll('bill');
+    $hd = 0;
+    $hdtc = 0;
+    while ($row5 = $res5->fetch_assoc()) {
+        if ($row5['status'] == 'Đã Nhận Hàng') {
             $hd++;
             $hdtc++;
-        }
-        else {
+        } else {
             $hd++;
         }
     }
 
-    if($hd == 0 || $hdtc == 0)
+    if ($hd == 0 || $hdtc == 0)
         $percent = 0;
     else
-        $percent=($hdtc/$hd)*100;
+        $percent = ($hdtc / $hd) * 100;
 
     $response = array(
         'countKH' => $countKH,
         'countSP' => $countSP,
         'countDHDD' => $countDHDD,
         'countSPBD' => $countSPBD,
-        'percent'=> $percent
+        'percent' => $percent
     );
     echo json_encode($response);
 }
-?>
