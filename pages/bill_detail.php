75cc11e540c37062c889d98078209bd98d973336
htuan2800 <huynhngoctuan48@gmail.com>
2024-04-18 20:18:03 +0700

    Update bill_detail.php

--- pages/bill_detail.php
+++ pages/bill_detail.php
@@ -2,6 +2,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd" crossorigin="anonymous">
     <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/bill.css">    
     <?php include "js/cart.php" ?>
 </head>
 
@@ -36,35 +37,35 @@ if ($purchase_data['status'] == "Đã Đặt") {
     $data .= " <div class='stepper'>
         <div class='stepper__step stepper__step--finish' aria-label='Đơn hàng $purchase_data[status], , $purchase_data[CreateTime]' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/order.svg'/>
+                <img style='width:40px;' src='img/status/order.svg'/>
             </div>
             <div class='stepper__step-text'>Đơn hàng $purchase_data[status]</div>
             <div class='stepper__step-date'>$purchase_data[CreateTime]</div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chờ xác nhận' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish background_step'>
-                <img style='width:40px;' src='images/status/money.svg'/>
+                <img style='width:40px;' src='img/status/money.svg'/>
             </div>
             <div class='stepper__step-text'>Chờ xác nhận</div>
             <div class='stepper__step-date'></div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chờ lấy hàng' tabindex='0'>
             <div class='stepper__step-icon2 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/truck.svg'/>
+                <img style='width:40px;' src='img/status/truck.svg'/>
             </div>
             <div class='stepper__step-text'>Chờ lấy hàng</div>
             <div class='stepper__step-date'></div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chờ giao hàng' tabindex='0'>
             <div class='stepper__step-icon2 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/box.svg'/>
+                <img style='width:40px;' src='img/status/box.svg'/>
             </div>
             <div class='stepper__step-text'>Chờ giao hàng</div>
             <div class='stepper__step-date'></div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chưa nhận hàng' tabindex='0'>
             <div class='stepper__step-icon2 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/receive.svg'/>
+                <img style='width:40px;' src='img/status/receive.svg'/>
             </div>
             <div class='stepper__step-text'>Chưa nhận hàng</div>
             <div class='stepper__step-date'></div>
@@ -77,35 +78,35 @@ if ($purchase_data['status'] == "Đã Đặt") {
     $data .= " <div class='stepper'>
         <div class='stepper__step stepper__step--finish' aria-label='Đơn hàng Đã Đặt, ,$purchase_data[CreateTime]' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/money.svg'/>
+                <img style='width:40px;' src='img/status/money.svg'/>
             </div>
             <div class='stepper__step-text'>Đơn hàng Đã Đặt</div>
             <div class='stepper__step-date'>$purchase_data[CreateTime]</div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Đơn hàng $purchase_data[status], , $purchase_data[UpdateTime]' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/order.svg'/>
+                <img style='width:40px;' src='img/status/order.svg'/>
             </div>
             <div class='stepper__step-text'>Đơn hàng $purchase_data[status]</div>
             <div class='stepper__step-date'>$purchase_data[UpdateTime]</div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chờ lấy hàng' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish background_step'>
-                <img style='width:40px;' src='images/status/truck.svg'/>
+                <img style='width:40px;' src='img/status/truck.svg'/>
             </div>
             <div class='stepper__step-text'>Chờ lấy hàng</div>
             <div class='stepper__step-date'></div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chờ giao hàng' tabindex='0'>
             <div class='stepper__step-icon2 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/box.svg'/>
+                <img style='width:40px;' src='img/status/box.svg'/>
             </div>
             <div class='stepper__step-text'>Chờ giao hàng</div>
             <div class='stepper__step-date'></div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chưa nhận hàng' tabindex='0'>
             <div class='stepper__step-icon2 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/receive.svg'/>
+                <img style='width:40px;' src='img/status/receive.svg'/>
             </div>
             <div class='stepper__step-text'>Chưa nhận hàng</div>
             <div class='stepper__step-date'></div>
@@ -118,35 +119,35 @@ if ($purchase_data['status'] == "Đã Đặt") {
     $data .= " <div class='stepper'>
         <div class='stepper__step stepper__step--finish' aria-label='Đơn hàng Đã Đặt, , $purchase_data[CreateTime]' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/order.svg'/>
+                <img style='width:40px;' src='img/status/order.svg'/>
             </div>
             <div class='stepper__step-text'>Đơn hàng Đã Đặt</div>
             <div class='stepper__step-date'>$purchase_data[CreateTime]</div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Đã Xác Nhận' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/money.svg'/>
+                <img style='width:40px;' src='img/status/money.svg'/>
             </div>
             <div class='stepper__step-text'>Đã Xác Nhận</div>
             <div class='stepper__step-date'></div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='$purchase_data[status], , $purchase_data[UpdateTime]' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/truck.svg'/>
+                <img style='width:40px;' src='img/status/truck.svg'/>
             </div>
             <div class='stepper__step-text'>$purchase_data[status]</div>
             <div class='stepper__step-date'>$purchase_data[UpdateTime]</div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chờ giao hàng' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish background_step'>
-                <img style='width:40px;' src='images/status/box.svg'/>
+                <img style='width:40px;' src='img/status/box.svg'/>
             </div>
             <div class='stepper__step-text'>Chờ giao hàng</div>
             <div class='stepper__step-date'></div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chưa nhận hàng' tabindex='0'>
             <div class='stepper__step-icon2 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/receive.svg'/>
+                <img style='width:40px;' src='img/status/receive.svg'/>
             </div>
             <div class='stepper__step-text'>Chưa nhận hàng</div>
             <div class='stepper__step-date'></div>
@@ -159,35 +160,35 @@ if ($purchase_data['status'] == "Đã Đặt") {
     $data .= " <div class='stepper'>
         <div class='stepper__step stepper__step--finish' aria-label='Đơn hàng Đã Đặt, , $purchase_data[CreateTime]' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/order.svg'/>
+                <img style='width:40px;' src='img/status/order.svg'/>
             </div>
             <div class='stepper__step-text'>Đơn hàng Đã Đặt</div>
             <div class='stepper__step-date'>$purchase_data[CreateTime]</div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chờ xác nhận' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/money.svg'/>
+                <img style='width:40px;' src='img/status/money.svg'/>
             </div>
             <div class='stepper__step-text'>Chờ xác nhận</div>
             <div class='stepper__step-date'></div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chờ lấy hàng' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/truck.svg'/>
+                <img style='width:40px;' src='img/status/truck.svg'/>
             </div>
             <div class='stepper__step-text'>Chờ lấy hàng</div>
             <div class='stepper__step-date'></div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='$purchase_data[status], , $purchase_data[UpdateTime]' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/box.svg'/>
+                <img style='width:40px;' src='img/status/box.svg'/>
             </div>
             <div class='stepper__step-text'>$purchase_data[status]</div>
             <div class='stepper__step-date'>$purchase_data[UpdateTime]</div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chưa nhận hàng' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish background_step'>
-                <img style='width:40px;' src='images/status/receive.svg'/>
+                <img style='width:40px;' src='img/status/receive.svg'/>
             </div>
             <div class='stepper__step-text'>Chưa nhận hàng</div>
             <div class='stepper__step-date'></div>
@@ -200,35 +201,35 @@ if ($purchase_data['status'] == "Đã Đặt") {
     $data .= " <div class='stepper'>
         <div class='stepper__step stepper__step--finish' aria-label='Đơn hàng Đã Đặt, , $purchase_data[CreateTime]' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/order.svg'/>
+                <img style='width:40px;' src='img/status/order.svg'/>
             </div>
             <div class='stepper__step-text'>Đơn hàng Đã Đặt</div>
             <div class='stepper__step-date'>$purchase_data[CreateTime]</div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chờ xác nhận' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/money.svg'/>
+                <img style='width:40px;' src='img/status/money.svg'/>
             </div>
             <div class='stepper__step-text'>Chờ xác nhận</div>
             <div class='stepper__step-date'></div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chờ lấy hàng' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/truck.svg'/>
+                <img style='width:40px;' src='img/status/truck.svg'/>
             </div>
             <div class='stepper__step-text'>Chờ lấy hàng</div>
             <div class='stepper__step-date'></div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='Chờ giao hàng' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/box.svg'/>
+                <img style='width:40px;' src='img/status/box.svg'/>
             </div>
             <div class='stepper__step-text'>Chờ giao hàng</div>
             <div class='stepper__step-date'></div>
         </div>
         <div class='stepper__step stepper__step--finish' aria-label='$purchase_data[status], , $purchase_data[UpdateTime]' tabindex='0'>
             <div class='stepper__step-icon1 stepper__step-icon--finish'>
-                <img style='width:40px;' src='images/status/receive.svg'/>
+                <img style='width:40px;' src='img/status/receive.svg'/>
             </div>
             <div class='stepper__step-text'>$purchase_data[status]</div>
             <div class='stepper__step-date'>$purchase_data[UpdateTime]</div>
@@ -243,46 +244,6 @@ if ($purchase_data['status'] == "Đã Đặt") {
     <?php
     echo $data;
     ?>
-    <!-- <div class="stepper">
-        <div class="stepper__step stepper__step--finish" aria-label="Đơn hàng đã đặt, , 09:54 03-02-2024" tabindex="0">
-            <div class="stepper__step-icon stepper__step-icon--finish">
-
-            </div>
-            <div class="stepper__step-text">Đơn hàng đã đặt</div>
-            <div class="stepper__step-date">09:54 03-02-2024</div>
-        </div>
-        <div class="stepper__step stepper__step--finish" aria-label="đã xác nhận thông tin thanh toán, , 10:24 03-02-2024" tabindex="0">
-            <div class="stepper__step-icon stepper__step-icon--finish">
-
-            </div>
-            <div class="stepper__step-text">đã xác nhận thông tin thanh toán</div>
-            <div class="stepper__step-date">10:24 03-02-2024</div>
-        </div>
-        <div class="stepper__step stepper__step--finish" aria-label="Đã giao cho ĐVVC, , 11:35 04-02-2024" tabindex="0">
-            <div class="stepper__step-icon stepper__step-icon--finish">
-
-            </div>
-            <div class="stepper__step-text">Đã giao cho ĐVVC</div>
-            <div class="stepper__step-date">11:35 04-02-2024</div>
-        </div>
-        <div class="stepper__step stepper__step--finish" aria-label="Đã nhận được hàng, , 16:39 07-02-2024" tabindex="0">
-            <div class="stepper__step-icon stepper__step-icon--finish">
-
-            </div>
-            <div class="stepper__step-text">Đã nhận được hàng</div>
-            <div class="stepper__step-date">16:39 07-02-2024</div>
-        </div>
-        <div class="stepper__step stepper__step--finish" aria-label="Đơn hàng đã hoàn thành, , 23:59 08-03-2024" tabindex="0">
-            <div class="stepper__step-icon stepper__step-icon--finish">
-
-            </div>
-            <div class="stepper__step-text">Đơn hàng đã hoàn thành</div>
-            <div class="stepper__step-date">23:59 08-03-2024</div>
-        </div>
-        <div class="stepper__line">
-            <div class="stepper__line-background" style="background: rgb(224, 224, 224);"></div>
-            <div class="stepper__line-foreground" style="width: calc(100% - 140px); background: rgb(45, 194, 88);"></div>
-        </div> -->
 </div>
 </div>
 <div class="container-fluid" id="main-content">
