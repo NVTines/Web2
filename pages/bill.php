1cb339ad8456fdb196485a2ccbac010671d33a52
htuan2800 <huynhngoctuan48@gmail.com>
2024-04-19 13:52:38 +0700

    Update bill.php

--- pages/bill.php
+++ pages/bill.php
@@ -22,38 +22,43 @@ if (isset($_SESSION['UserID'])) {
         if (mysqli_num_rows($res1) > 0) {
             $row1 = $res1->fetch_assoc();
             $res2 = $dtb->select("SELECT * FROM `bill` WHERE `CustomerID`=?", [$row1['CustomerID']], 'i');
-            while ($row2 = mysqli_fetch_assoc($res2)) {
-                $dataCart = "";
-                $res3 = $dtb->select("SELECT * FROM `billdetail` WHERE `BillID`=?", [$row2['BillID']], 'i');
-                while ($row3 = mysqli_fetch_assoc($res3)) {
-                    $res4 = $dtb->select("SELECT * FROM `product` WHERE `ProductID`=?", [$row3['ProductID']], 'i');
-                    if (mysqli_num_rows($res4) > 0) {
-                        $row4 = $res4->fetch_assoc();
-                        $imgBase64 = base64_encode($row4['IMG']);
-                        // Tạo đường dẫn dữ liệu (data URL) cho thẻ <img>
-                        $imgSrc = 'data:image/jpeg;base64,' . $imgBase64;
-                        $dataCart .= "<div class='card-body border border-black m-2 d-flex justify-content-between'>
-                            <div>
-                            <h5 class='card-title'>$row4[ProductName] x $row3[Quantity]</h5>
-                            <p class='card-text'>$row3[Unitprice]$</p>
-                            </div>
-                            <img src='$imgSrc' class='img-fluid' style='width:60px;'>
-                        </div>";
-                    }
-                }
-
-                echo <<<donhang
-                        <div class="ms-auto p-4 overflow-hidden">
-                            <div class="card">
-                                <h5 class="card-header text-end">Tình Trạng Đơn Hàng: $row2[status]</h5>
+            if(mysqli_num_rows($res2)>0){
+                while ($row2 = mysqli_fetch_assoc($res2)) {
+                    $dataCart = "";
+                    $res3 = $dtb->select("SELECT * FROM `billdetail` WHERE `BillID`=?", [$row2['BillID']], 'i');
+                    while ($row3 = mysqli_fetch_assoc($res3)) {
+                        $res4 = $dtb->select("SELECT * FROM `product` WHERE `ProductID`=?", [$row3['ProductID']], 'i');
+                        if (mysqli_num_rows($res4) > 0) {
+                            $row4 = $res4->fetch_assoc();
+                            $imgBase64 = base64_encode($row4['IMG']);
+                            // Tạo đường dẫn dữ liệu (data URL) cho thẻ <img>
+                            $imgSrc = 'data:image/jpeg;base64,' . $imgBase64;
+                            $dataCart .= "<div class='card-body border border-black m-2 d-flex justify-content-between'>
                                 <div>
-                                $dataCart
+                                <h5 class='card-title'>$row4[ProductName] x $row3[Quantity]</h5>
+                                <p class='card-text'>$row3[Unitprice]$</p>
                                 </div>
-                                <a href='index.php?page=bill_detail&bill_id=$row2[BillID]' class='btn edit text-center text-success'>Xem Chi Tiết</a> 
+                                <img src='$imgSrc' class='img-fluid' style='width:60px;'>
+                            </div>";
+                        }
+                    }
+    
+                    echo <<<donhang
+                            <div class="ms-auto p-4 overflow-hidden">
+                                <div class="card">
+                                    <h5 class="card-header text-end">Tình Trạng Đơn Hàng: $row2[status]</h5>
+                                    <div>
+                                    $dataCart
+                                    </div>
+                                    <a href='index.php?page=bill_detail&bill_id=$row2[BillID]' class='btn edit text-center text-success'>Xem Chi Tiết</a> 
+                                </div>
                             </div>
-                        </div>
-                    donhang;
+                        donhang;
+                }
+            } else {
+                echo "<div class='mt-5 mb-5'><p class='text-center text-danger'>Bạn chưa có đơn hàng nào trong dữ liệu</p></div>";
             }
+            
         }
 
         ?>
