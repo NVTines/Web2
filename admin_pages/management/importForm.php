b456ee56d4a234fc6118ba04ecf060f6c9e27190
Khánh Phạm Phú <phamphukhanh.sgu.edu@gmail.com>
2024-04-29 14:26:06 +0700

    add import product php file

--- admin_pages/management/importForm.php
+++ admin_pages/management/importForm.php
@@ -7,7 +7,7 @@
 <div class="col-div-8">
     <div class="box-8">
         <div class="box-8-body">
-            <form action="./functions/.php" class="info-manage-form" method="POST">
+            <form action="./functions/importProducts.php" class="info-manage-form" method="POST">
                 <div style="margin-left:25%;margin-right:25%;margin-top:100px;">
                     <div class="info-manage-wrapper">
                         <label class="info-manage-label" for="supplier">Nhà cung cấp:</label>
