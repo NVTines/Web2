1d3e7abcc8b210995daf6fb289a80a528f17bca5
htuan2800 <huynhngoctuan48@gmail.com>
2024-05-02 19:34:40 +0700

    Create header.php

---
+++ pages/header.php
@@ -0,0 +1,2 @@
+<img class="slideimg" src="https://ragus.vn/wp-content/uploads/2020/01/cach-ch%E1%BB%8Dn-giay-chay-bo-phu-hop-10.jpg" style="width: 100%" />
+<img class="slideimg" src="https://www.elleman.vn/wp-content/uploads/2020/01/10/01_giaychaybo_ELLE-Man_01-20.jpg" style="width: 100%" />
