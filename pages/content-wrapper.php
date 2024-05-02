c312e08308cc89e6dc4cfc23f5b8c66309209481
htuan2800 <huynhngoctuan48@gmail.com>
2024-05-02 19:34:09 +0700

    Create content-wrapper.php

---
+++ pages/content-wrapper.php
@@ -0,0 +1,63 @@
+<?php   
+    if(isset($_SESSION['UserID'])){
+        $loginCheck = true;
+    }else{
+        $loginCheck = false;
+    }
+    if(isset($_GET['page'])){
+        $page_name = $_GET['page'];
+        switch($page_name){
+            case 'news':
+                require "pages/news-content/news.php";
+                break;
+            case 'news1':
+                require "pages/news-content/news1.php";
+                break;
+            case 'news2':
+                require "pages/news-content/news2.php";
+                break;
+            case 'news3':
+                require "pages/news-content/news3.php";
+                break;
+            case 'news7':
+                require "pages/news-content/news7.php";
+                break;
+            case 'news8':
+                require "pages/news-content/news8.php";
+                break;
+            case 'news9':
+                require "pages/news-content/news9.php";
+                break;
+            case 'shopping':
+                if(isset($_GET['id'])){
+                    require "pages/productDetail.php";
+                }else
+                    require "pages/shopping.php";
+                break;
+            case 'urcart':
+                require "pages/cart.php";
+                break;
+            case 'urbill';
+                require "pages/bill.php";
+                break;
+            case 'bill_detail';
+                require "pages/bill_detail.php";
+                break;
+            case 'info':
+                if($loginCheck){
+                    require "pages/info.php";
+                }else{
+                    require "pages/error/error.php";
+                }
+                break;
+        }
+    }else{
+        echo '<div class="header">';
+        require "pages/header.php";
+        echo '</div><div class="row">'; 
+        require "pages/content.php";
+        echo '</div><div class="beforefooter" id="beforefooter">';
+        require "pages/beforefooter.php";
+        echo '</div>';
+    }
+?>
