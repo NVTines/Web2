61aa07055748dd693ddf93f4158bcaa6bf05049e
htuan2800 <huynhngoctuan48@gmail.com>
2024-05-02 19:34:43 +0700

    Create leftcontent.php

---
+++ pages/leftcontent.php
@@ -0,0 +1,10 @@
+<div class="card">
+    <h2><a href="pages/news.html" class="a">Meet the LeBron James Innovation Center</a></h2>
+    <h5>SPORT SCIENCE</h5>
+    <a href="pages/news.html"><input type="button" style="background-image:url(https://s3.amazonaws.com/nikeinc/assets/104654/Nike_Sports_Research_Lab_LeBron_James_Innovation_Center_8_native_1600.jpg?1631723712);width: 100%; height: 600px;background-size: cover;cursor:pointer" value=""/></a>
+</div>
+<div class="card">
+    <h2><a href="pages/news1.html" class="a">Converse and Rick Owens Continue DRKSHDW Collaboration</a></h2>
+    <h5>SEE THE WORLD WITH DIFFERENT ASPECTS</h5>
+    <a href="pages/news1.html"><input type="button" style="background-image:url(https://s3.amazonaws.com/nikeinc/assets/103711/Converse_x_DRKSHDW_TurboDrk__172345C_5_square_1600.jpg?1626948331);width: 100%; height: 800px;background-size: cover;cursor:pointer" value=""/></a>
+</div>
