a58fea0acf547432a5ab6b4b1114549febf2fd88
htuan2800 <huynhngoctuan48@gmail.com>
2024-05-02 19:34:21 +0700

    Create footer.php

---
+++ pages/footer.php
@@ -0,0 +1,20 @@
+<div class="footer-content" style="font-family:Arial, Helvetica, sans-serif;">
+    <div class="sub-heading">Made By NIT</div>
+    <div class="slogan">Chasing Your Dream, Make Your Own Day.<br>New Shoes Are Always A Good Idea! Thank You For Buying!</div>
+    <ul class="footer-social">
+        <li class="social-media"></li>
+        <a class="social-popup-icon" target="_blank" style="font-size:18px;color:white">
+            <i class="fab fa-facebook-f"></i> https://www.facebook.com/nvtins
+        </a>
+
+        <li class="social-media"></li>
+        <a class="social-popup-icon" target="_blank" style="font-size:18px;color:white">
+            <i class="fab fa-twitter"></i> https://twitter.com/nvtines
+        </a>
+
+        <li class="social-media"></li>
+        <a class="social-popup-icon" target="_blank" style="font-size:18px;color:white">
+            <i class="fab fa-instagram"></i> https://www.instagram.com/nggvt/
+        </a>
+    </ul>
+</div>
