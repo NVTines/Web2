f922328797aa6a57dcac3ac4adbcb214bfcc5b72
htuan2800 <huynhngoctuan48@gmail.com>
2024-05-02 19:34:16 +0700

    Create error.php

---
+++ pages/error/error.php
@@ -0,0 +1,8 @@
+<div class="error-wrapper" id="error-wrapper">
+    <div class="error-banner" id="error-banner">;
+        <img src="img/error.jpg" />
+        <h1>Access Denied</h1>
+        <h3>Hãy đăng nhập và thử lại !</h3>
+        <a href="index.php"><button id="homepage-btn" class="homepage-btn">GO TO HOME</button></a>
+    </div>
+</div>
