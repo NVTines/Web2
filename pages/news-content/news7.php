874b3235d25b6be3b23417e7329e5d2b89cc1981
htuan2800 <huynhngoctuan48@gmail.com>
2024-05-02 19:34:59 +0700

    Create news7.php

---
+++ pages/news-content/news7.php
@@ -0,0 +1,31 @@
+<div style="padding-top: 100px">
+    <div style="text-align: center;font-weight: bold;text-decoration: underline;padding-right: 1150px">MAKE WITH RECYCLED MATERIALS</div>
+    <div style="text-align: center;font-size: 35px"><h2>ECO-INNOVATING WITH PARLEY AND REBO IN THE FIGHT AGAINST MARINE PLASTIC POLLUTION</h2>
+    </div>
+    <div style="text-align: center"><i>June 3, 2021</i></div>
+</div>
+<div>
+    <img src="https://preview.thenewsmarket.com/Previews/ADID/StillAssets/920x690/585892.jpg" style="width: 65%;padding: 40px;float: left;clear: both"/>
+</div>
+<div style="padding: 40px;text-align: center;font-family: Gotham, 'Helvetica Neue', Helvetica, Arial, 'sans-serif';text-decoration: underline">
+    <p><h3>WE ARE IN HURRY</h3></p>
+</div>
+<div style="padding: 40px;text-align: center">
+    <img src="https://preview.thenewsmarket.com/Previews/ADID/StillAssets/320x320/585894.jpg" style="width: 15%;height: 215px"/>
+    <img src="https://preview.thenewsmarket.com/Previews/ADID/StillAssets/320x320/585895.jpg" style="width: 15%;height: 215px"/>
+    <img src="https://preview.thenewsmarket.com/Previews/ADID/StillAssets/400x300/585896_v3.jpg" style="width: 15%;height: 215px"/>
+    <img src="https://preview.thenewsmarket.com/Previews/ADID/StillAssets/320x320/585893.jpg" style="width: 15%;height: 215px"/>
+</div>
+<div style="font-size: 20px">
+    <p style="width: 65%;padding: 40px;text-indent: 50px">This year will mark the fourth year of adidas x Parley Run for the Oceans. To-date, adidas x Parley Run for the Oceans has united over 3 million runners around the world, collectively clocking up 25.5 million KMs and raising $2.5 million to help Parley develop its initiatives and educational programs in the fight against marine plastic pollution. In 2019, 92 schools were enrolled in the program and are now educations hubs, as well as plastic interception points for the local community. As a result, 70,000 kids and their families participated in the program.</p>
+    <p style="width: 65%;padding: 40px;font-weight: bold;font-size: 30px">"AS A GLOBAL BRAND, AND AS GLOBAL CITIZENS, WE HAVE A BIG ROLE TO PLAY IN CONSERVING THE OCEANS AND OUR PLANET. IT IS OUR RESPONSIBILITY TO USE OUR PLATFORMS, PRODUCTS, AND VOICES TO NOT JUST RAISE AWARENESS, BUT TO PUT INTO EFFECT REAL ACTION."</p>
+    <p style="width: 65%;padding: 40px;text-indent: 50px">Once again, empowering runners worldwide to make a difference, for every kilometre ran between May 28 – June 8 via the adidas Running app (Strava/Joyrun), together with Parley we will clean up the equivalent weight of 10 plastic bottles, up to a maximum of 500,000 lbs of marine plastic waste from beaches, remote islands, and coastlines.</p>
+    <img src="https://preview.thenewsmarket.com/Previews/ADID/StillAssets/640x480/585893.jpg" style="width: 50%;margin: 80px"/>
+    <p style="width: 65%;padding: 40px;font-weight: bold;font-size: 30px">"MORE THAN A PARTNER, ADIDAS HAS BEEN A TRUE ALLY, THE FAMILY MEMBER WE SO URGENTLY NEEDED ON OUR SIDE TO PROVE THAT INDUSTRY CAN BE THE KEY THAT SCALES AND ACCELERATES SOLUTIONS TO THE MASSIVE ENVIRONMENTAL PROBLEMS WE FACE.</p>
+    <p style="width: 65%;padding: 40px;font-weight: bold;font-size: 30px">WE BELIEVE THAT COLLABORATION, CREATIVITY, AND ECO-INNOVATION IS THE WINNING FORMULA TO SAVE OUR OCEANS AND END THE GLOBAL PLASTIC CRISIS THREATENING THE NATURAL WORLD, OUR OWN HEALTH AND OUR FUTURE. EVERYONE WHO JOINS THE RUN HELPS DRIVE THIS VISION FORWARD."</p>
+    <p style="width: 65%;padding: 40px;text-indent: 50px">And now, for the first time, scaling the initiative beyond June 8, we and Parley have partnered with REBO to introduce a limited-edition adidas x REBO smart water bottle in our continued fight to help end plastic waste.</p>
+    <img src="https://preview.thenewsmarket.com/Previews/ADID/StillAssets/640x480/585896_v3.jpg" style="width: 50%;margin: 80px"/>
+    <p style="width: 65%;padding: 40px;text-indent: 50px">REBO’s interconnected technology embedded in the cap tracks the water you drink, the plastic bottles avoided, and issues plastic credits to pay the cost of collecting tossed away plastic bottles. 1 bottle drunk = 1 plastic bottle collected. The REBO app is synched to the Runtastic Adidas Running app to dynamically recommend optimal hydration needs. The plastic collection cost is paid by REBO and its sponsors and goes to the Parley Global Cleanup Network, an alliance of organizations that collaborate to remove plastic waste from beaches, remote islands, rivers, mangroves in coastal communities.</p>
+    <p style="width: 65%;padding: 40px;font-weight: bold;font-size: 30px">"WE ARE RUNNING OUT OF TIME TO TURN THE TIDE ON PLASTIC WASTE AND TECHNOLOGY CAN HELP CHANGE THE COURSE.  WITH THE LIMITED-EDITION REBO X ADIDAS X PARLEY SMART BOTTLE WE CAN BECOME FOOT-SOLDIERS IN THE FIGHT AGAINST PLASTIC WASTE.</p>
+    <p style="width: 65%;padding: 40px;font-weight: bold;font-size: 30px;margin-bottom: 100px">EVERY SIP TO STAY HYDRATED CONTRIBUTES TO CLEANING THE PLANET FROM PLASTIC WASTE. JOIN RUN FOR THE OCEANS AND THE HYDRATION REVOLUTION."</p>
+</div>
