2313be3f27c1dc3f134dfeb01733f4a442cc6ad7
htuan2800 <huynhngoctuan48@gmail.com>
2024-05-02 19:31:47 +0700

    Create content.php

---
+++ admin_pages/content.php
@@ -0,0 +1,27 @@
+<?php
+if (isset($_GET['key'])) {
+  echo '<link rel="stylesheet" href="../css/user.css" type="text/css" />';
+  $key = $_GET['key'];
+  switch ($key) {
+    case 'users':
+      require 'management/users.php';
+      break;
+    case 'sp':
+      require 'management/sanpham.php';
+      break;
+    case 'dh':
+      require 'management/donhang.php';
+      break;
+    case 'nh':
+      require 'management/import.php';
+      break;
+    case 'ncc':
+      require 'management/supplier.php';
+      break;
+    case 'q':
+      require 'management/role.php';
+      break;
+  }
+} else {
+  require 'management/dashboard.php';
+}
