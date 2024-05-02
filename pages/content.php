11fd7bc65b77e51efc28d9ce16cfb3dff2e028fe
htuan2800 <huynhngoctuan48@gmail.com>
2024-05-02 19:34:11 +0700

    Create content.php

---
+++ pages/content.php
@@ -0,0 +1,31 @@
+<div class="leftcolumn">
+    <div class="card">
+        <h2><a href="index.php?page=news" class="a">Meet the LeBron James Innovation Center</a></h2>
+        <h5>SPORT SCIENCE</h5>
+        <a href="index.php?page=news"><input class="img_news" type="button" style="background-image:url(https://s3.amazonaws.com/nikeinc/assets/104654/Nike_Sports_Research_Lab_LeBron_James_Innovation_Center_8_native_1600.jpg?1631723712);width: 100%; height: 600px;" value=""/></a>
+    </div>
+    <div class="card">
+        <h2><a href="index.php?page=news1" class="a">Converse and Rick Owens Continue DRKSHDW Collaboration</a></h2>
+        <h5>SEE THE WORLD WITH DIFFERENT ASPECTS</h5>
+        <a href="index.php?page=news1"><input class="img_news" type="button" style="background-image:url(https://s3.amazonaws.com/nikeinc/assets/103711/Converse_x_DRKSHDW_TurboDrk__172345C_5_square_1600.jpg?1626948331);width: 100%; height: 800px;" value=""/></a>
+    </div>
+</div>
+<div class="rightcolumn">
+    <div class="card">
+        <a href="index.php?page=news2"><input class="img_news" type="button" style="background-image:url(https://s3.amazonaws.com/nikeinc/assets/104850/fy22_q1_earnings_1920x1080_NikeNews_native_1000.jpg?1632413276);width: 100%; height:320px;" value=""/></a>
+        <a href="index.php?page=news2" class="aboutme">
+            <h3>NIKE, Inc. Reports Fiscal 2022 First Quarter Results</h3>
+        </a>
+    </div>
+    <div class="card">
+        <a href="index.php?page=news3"><input class="img_news" type="button" style="background-image:url(https://s3.amazonaws.com/nikeinc/assets/104694/NikeNews_AirMax90_LHM_1_hd_1600.jpg?1631904523);width: 100%; height:320px;" value=""/></a>
+        <a href="index.php?page=news3" class="aboutme">
+            <h3>
+                The Air Max 90 LHM Puts Family First
+            </h3>
+        </a>
+    </div>
+    <div class="poster">
+        <img style="width: 100%;border-radius: 25px" src="https://images.summitmedia-digital.com/spotph/images/2020/09/04/nikepark-eoss-sale-sept-2020-poster-1599216446.jpg"/>
+    </div>
+</div>
