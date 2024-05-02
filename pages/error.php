c4e3f3dc76b1e0611207f0d1e015ed7e65d0aa92
htuan2800 <148872981+htuan2800@users.noreply.github.com>
2024-05-02 19:25:16 +0700

    Delete pages directory

--- pages/error.php
+++
@@ -1,8 +0,0 @@
-<div class="error-wrapper" id="error-wrapper">
-    <div class="error-banner" id="error-banner">;
-        <img src="img/error.jpg" />
-        <h1>Access Denied</h1>
-        <h3>Please login and try again !</h3>
-        <a href="index.php"><button id="homepage-btn" class="homepage-btn">GO TO HOME</button></a>
-    </div>
-</div>
--- pages/header.php
+++
@@ -1,2 +0,0 @@
-<img class="slideimg" src="https://ragus.vn/wp-content/uploads/2020/01/cach-ch%E1%BB%8Dn-giay-chay-bo-phu-hop-10.jpg" style="width: 100%" />
-<img class="slideimg" src="https://www.elleman.vn/wp-content/uploads/2020/01/10/01_giaychaybo_ELLE-Man_01-20.jpg" style="width: 100%" />
