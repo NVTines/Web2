3be751459e02e8a7a557ec0fd8bd5d5efe69b6a6
htuan2800 <huynhngoctuan48@gmail.com>
2024-05-03 11:09:37 +0700

    Update cart.php

--- js/cart.php
+++ js/cart.php
@@ -1,6 +1,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script>
-    function alert(type, msg, position = 'body') {
+    function alertCustom(type, msg, position = 'body') {
+        console.log(msg)
         let bs_class = (type == 'success') ? 'alert-success' : 'alert-danger';
         let element = document.createElement('div');
         element.innerHTML = `
@@ -28,11 +29,11 @@
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xhr.onload = function() {
             if (this.responseText == 1) {
-                // alert('success', 'Cart Added!', 'cart-alert');
+                // alertCustom('success', 'Cart Added!', 'cart-alert');
                 console.log(this.responseText);
                 //get_product_cart(user_id);
             } else {
-                // alert('error', 'Add cart failed!', 'cart-alert');
+                // alertCustom('error', 'Add cart failed!', 'cart-alert');
                 console.log(this.responseText);
             }
         }
@@ -44,10 +45,10 @@
     userhoadon_form.addEventListener('submit', (e) => {
         e.preventDefault();
         if (userhoadon_form.elements['pttt'].value == 0) {
-            alert('warning', "Chưa chọn phương thức thanh toán");
+            alertCustom('warning', "Chưa chọn phương thức thanh toán");
 
         } else if (userhoadon_form.elements['delivery'].value == '') {
-            alert('warning', "Mời nhập địa chỉ giao hàng");
+            alertCustom('warning', "Mời nhập địa chỉ giao hàng");
         } else {
             let data = new FormData();
             let paymentValue = userhoadon_form.elements['pttt'].value;
@@ -65,15 +66,15 @@
                 var modal = bootstrap.Modal.getInstance(myModal);
                 modal.hide();
                 if (this.responseText == "GHR") {
-                    alert('warning', "Giỏ hàng rỗng!");
+                    alertCustom('warning', "Giỏ hàng rỗng!");
                 } else if (this.responseText == 1) {
                     userhoadon_form.reset();
-                    alert('success', "Đơn hàng đã được đặt!");
+                    alertCustom('success', "Đơn hàng đã được đặt!");
                     setTimeout(function() {
                         location.reload();
                     }, 3000);
                 } else if (this.responseText == "Server Down!") {
-                    alert('danger', "Server lỗi!");
+                    alertCustom('danger', "Server lỗi!");
                 }
             }
             xhr.send(data);
@@ -103,9 +104,9 @@
         xhr.open("POST", "pages/functions/addToCart.php", true);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xhr.onload = function() {
-        let response = JSON.parse(this.responseText);
-        document.getElementById('cart-data').innerHTML = response.cartData;
-        document.getElementById('thanhtoan').innerHTML = response.thanhtoan;
+            let response = JSON.parse(this.responseText);
+            document.getElementById('cart-data').innerHTML = response.cartData;
+            document.getElementById('thanhtoan').innerHTML = response.thanhtoan;
         }
         xhr.send('get_product_cart' + '&cart_id=' + cart_id);
     }
@@ -120,40 +121,39 @@
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xhr.onload = function() {
             if (this.responseText == 1) {
-                alert('success', "Xóa Thành Công!");
+                alertCustom('success', 'Xóa Thành Công!');
                 get_product_cart(cartID);
             } else {
-                alert('danger', "Server lỗi!");
+                alertCustom('danger', 'Server lỗi!');
             }
         }
-        xhr.send('remove_product' + '&product_id=' + product_id + '&cart_id=' + cartID + '&size_id='+sizeID);
+        xhr.send('remove_product' + '&product_id=' + product_id + '&cart_id=' + cartID + '&size_id=' + sizeID);
     }
 
-    function setQuantityPlus(cartID, product_id,max_quantity, size_id) {
-        let quantity = document.getElementById('quantity' + product_id +size_id).value;
+    function setQuantityPlus(cartID, product_id, max_quantity, sizeID) {
+        let quantity = document.getElementById('quantity' + product_id + sizeID).value;
         quantity = parseInt(quantity);
-        if(max_quantity < quantity +1){
-            alert('danger', "Không đủ số lượng hàng");
-        }
-        else {
+        if (max_quantity < quantity + 1) {
+            alertCustom('danger', 'Không đủ số lượng hàng');
+        } else {
             quantity = quantity + 1;
             let xhr = new XMLHttpRequest();
             xhr.open("POST", "pages/functions/addToCart.php", true);
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
             xhr.onload = function() {
                 if (this.responseText == 1) {
-                    alert('success', "Câp nhật thành công!");
+                    alertCustom('success', 'Câp nhật thành công!');
                     get_product_cart(cartID);
                 } else {
-                    alert('danger', "Server lỗi!");
+                    alertCustom('danger', 'Server lỗi!');
                 }
             }
-            xhr.send('update_quantity' + '&product_id=' + product_id + '&quantity=' + quantity +'&size_id=' + size_id);
+            xhr.send('update_quantity' + '&product_id=' + product_id + '&quantity=' + quantity + '&size_id=' + sizeID);
         }
     }
 
-    function setQuantityMinus(cartID, product_id, size_id) {
-        let quantity = document.getElementById('quantity' + product_id+size_id).value;
+    function setQuantityMinus(cartID, product_id, sizeID) {
+        let quantity = document.getElementById('quantity' + product_id + sizeID).value;
         quantity = parseInt(quantity);
 
         if (quantity > 0) {
@@ -163,13 +163,13 @@
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
             xhr.onload = function() {
                 if (this.responseText == 1) {
-                    alert('success', "Câp nhật thành công!");
+                    alertCustom('success', 'Câp nhật thành công!');
                     get_product_cart(cartID);
                 } else {
-                    alert('danger', "Server lỗi!");
+                    alertCustom('danger', 'Server lỗi!');
                 }
             }
-            xhr.send('update_quantity' + '&product_id=' + product_id + '&quantity=' + quantity + '&size_id=' + size_id);
+            xhr.send('update_quantity' + '&product_id=' + product_id + '&quantity=' + quantity + '&size_id=' + sizeID);
         }
     }
 </script>
