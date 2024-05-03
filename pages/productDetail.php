4aa8ee66fa4054c54ecffcf952a9c65391138c6d
htuan2800 <huynhngoctuan48@gmail.com>
2024-05-03 11:10:25 +0700

    Update productDetail.php

--- pages/productDetail.php
+++ pages/productDetail.php
@@ -18,16 +18,12 @@ if (isset($_GET['id'])) {
 ?>
 
     <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Product Details</title>
+      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script> 
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
       <link rel="stylesheet" href="css/common.css">
-
     </head>
     <script>
       document.querySelector("input").addEventListener("keypress", function(evt) {
@@ -135,7 +131,7 @@ if (isset($_GET['id'])) {
     </div>
 
     <script>
-      function alert(type, msg, position = 'body') {
+      function alertCustom(type, msg, position = 'body') {
         let bs_class = (type == 'success') ? 'alert-success' : 'alert-danger';
         let element = document.createElement('div');
         element.innerHTML = `
@@ -163,11 +159,11 @@ if (isset($_GET['id'])) {
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xhr.onload = function() {
           if (this.responseText == 1) {
-            // alert('success', 'Cart Added!', 'cart-alert');
+            // alertCustom('success', 'Cart Added!', 'cart-alert');
             console.log(this.responseText);
             //get_product_cart(user_id);
           } else {
-            // alert('error', 'Add cart failed!', 'cart-alert');
+            // alertCustom('error', 'Add cart failed!', 'cart-alert');
             console.log(this.responseText);
           }
         }
@@ -189,12 +185,12 @@ if (isset($_GET['id'])) {
           xhr.open("POST", "pages/functions/addToCart.php", true);
 
           xhr.onload = function() {
-            if (this.responseText == "u_success") {
-              alert('success', "Số lượng được cập nhật!");
+            if (this.responseText == "added") {
+              alertCustom('warning', "Sản phẩm đã được thêm!");
             } else if (this.responseText == 1) {
               location.reload();
             } else if (this.responseText == "Server Down!") {
-              alert('danger', "Server lỗi!");
+              alertCustom('danger', "Server lỗi!");
             }
           }
           xhr.send(data);
